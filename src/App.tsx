2ddd1fe5d75b6f19815a6f08f2cd48a849e80acd
Ryan <ryancode2321@gmail.com>
2026-03-23 10:31:44 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+	return (
+		<main>
+			<div>
+				<h1 className="text-3xl font-bold underline">Hello world!</h1>
+			</div>
+		</main>
+	);
+}
+
+export default App;
