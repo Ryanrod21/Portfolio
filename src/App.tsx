40d488b6f1aacf4f21f896b2753db53269f9c92e
Ryan <ryancode2321@gmail.com>
2026-08-11 09:24:03 -0500

    Moved tech to the bottom

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
-import './App.css';
-import Hero from './sections/Hero';
-import Projects from './sections/Projects';
-import Tech from './sections/Tech';
-import Contact from './sections/Contact';
-import About from './sections/About';
-import { Footer } from './sections/Footer';
-import WorkingOn from './sections/WorkingOn';
+import "./App.css";
+import Hero from "./sections/Hero";
+import Projects from "./sections/Projects";
+import Tech from "./sections/Tech";
+import Contact from "./sections/Contact";
+import About from "./sections/About";
+import { Footer } from "./sections/Footer";
+import WorkingOn from "./sections/WorkingOn";
 
 function App() {
-  return (
-    <main className="bg-black max-w-8xl mx-auto">
-      <Hero />
-      <About />
-      <Tech />
-      <Projects />
-      <WorkingOn />
-      <Contact />
-      <Footer />
-    </main>
-  );
+	return (
+		<main className="bg-black max-w-8xl mx-auto">
+			<Hero />
+			<About />
+			<Projects />
+			<WorkingOn />
+			<Tech />
+			<Contact />
+			<Footer />
+		</main>
+	);
 }
 
 export default App;
